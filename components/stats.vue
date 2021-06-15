a3fa878fbd660f0ef71b4da88952b1574e811aef
kaleemubarok <barikly.mubarok@gmail.com>
2021-06-15 07:59:35 +0000

    add number animation

--- components/stats.vue
+++ components/stats.vue
@@ -5,33 +5,88 @@
     <div
       class="bg-red-500 dark:bg-indigo-800 transition-all duration-300 text-white dark:text-red-500 px-4 py-10 rounded-lg shadow"
     >
-      <p class="text-3xl font-bold">{{ stats.positif }}</p>
+      <p class="text-3xl font-bold">
+        <number
+        class="bold transition"
+        :class="{scaleBig: scaleClass}"
+        ref="number2"
+        :from=stats.positif-50
+        :format="theFormat"
+        :to=stats.positif
+        :duration="duration"
+        easing="Circ.easeIn"/>
+      </p>
       <p>Terkonfirmasi</p>
     </div>
     <div
       class="bg-blue-500 dark:bg-indigo-800 transition-all duration-300 text-white dark:text-blue-500 px-4 py-10 rounded-lg shadow"
     >
-      <p class="text-3xl font-bold">{{ stats.dirawat }}</p>
+      <p class="text-3xl font-bold">
+        <number
+        class="bold transition"
+        :class="{scaleBig: scaleClass}"
+        ref="number2"
+        :from=stats.dirawat-20
+        :format="theFormat"
+        :to=stats.dirawat
+        :duration="duration"
+        easing="Circ.easeIn"/>
+      </p>
       <p>Dirawat</p>
     </div>
     <div
       class="bg-green-500 dark:bg-indigo-800 transition-all duration-300 text-white dark:text-green-500 px-4 py-10 rounded-lg shadow"
     >
-      <p class="text-3xl font-bold">{{ stats.sembuh }}</p>
+      <p class="text-3xl font-bold">
+        <number
+        class="bold transition"
+        :class="{scaleBig: scaleClass}"
+        ref="number2"
+        :from=stats.sembuh-20
+        :format="theFormat"
+        :to=stats.sembuh
+        :duration="duration"
+        easing="Circ.easeIn"/>
+      </p>
       <p>Sembuh</p>
     </div>
     <div
       class="bg-gray-600 dark:bg-indigo-800 transition-all duration text-white  px-4 py-10 rounded-lg shadow"
     >
-      <p class="text-3xl font-bold">{{ stats.meninggal }}</p>
+      <p class="text-3xl font-bold">
+        <number
+        class="bold transition"
+        :class="{scaleBig: scaleClass}"
+        ref="number2"
+        :from=stats.meninggal-20
+        :format="theFormat"
+        :to=stats.meninggal
+        :duration="duration"
+        easing="Circ.easeOut"/>
+      </p>
       <p>Meninggal</p>
     </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import VueNumber from 'vue-number-animation'
+ 
+Vue.use(VueNumber)
 export default {
-  props: ["stats"]
+  props: ["stats"],
+  data() {
+    return {
+      duration: 3,
+      scaleClass: false
+    };
+  },
+  methods: {
+    theFormat(number) {
+      return new Intl.NumberFormat('id-ID').format(number);
+    },
+  }
 };
 </script>
 
